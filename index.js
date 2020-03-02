0657879475cc8380ba847f9136eadf1d598a8026
jonoaugustine <jonoaugustine@jhu.edu>
2020-03-02 18:34:41 -0500

    setup

---
+++ index.js
@@ -0,0 +1,8 @@
+const afkElement = document.getElementById("afk");
+const textAfk = "Jono is AFK or unavailable";
+const textNotAfk = "Jono is not AFK";
+
+/** @returns {boolean} */
+const isAfk = () => {};
+
+setInterval(() => {}, 1000 * 60);
